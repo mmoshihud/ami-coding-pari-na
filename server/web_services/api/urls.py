c78f44094c2635f45b69d85d9ecc786157b9f474
Munshi Moshihud Hossain <18-39125-3@student.aiub.edu>
2023-08-14 14:51:16 +0600

    authentication done login, registration, logout

--- server/web_services/api/urls.py
+++ server/web_services/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from web_services.api.views import create_user
+from web_services.api.views import create_user, logout
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("login", obtain_auth_token),
     path("user", create_user),
+    path("logout", logout),
 ]
--- server/web_services/api/views.py
+++ server/web_services/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from web_services.api.serializers import UserSerializer
 from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 
 @api_view(["POST"])
@@ -11,13 +12,17 @@ def create_user(request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-
             token, created = Token.objects.get_or_create(user=user)
-
             response_data = {
                 "user": serializer.data,
                 "token": token.key,
             }
-
             return Response(response_data, status=201)
         return Response(serializer.errors, status=400)
+
+
+@api_view(["POST"])
+def logout(request):
+    if request.method == "POST":
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
